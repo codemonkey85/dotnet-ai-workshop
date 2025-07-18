0693ee8a28bd1e4c7f58f7a3f58f69f6d6700f89
Michael Bond <michael@bondcodes.com>
2025-07-18 11:05:53 -0400

    Run through Part 1

--- exercises/1. QuizApp/Begin/Program.cs
+++ exercises/1. QuizApp/Begin/Program.cs
@@ -5,10 +5,27 @@ using System.ClientModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// TODO:
 //  - Decide which LLM backend you're going to use, and register it in DI
 //  - It can be any IChatClient implementation, for example AzureOpenAIClient or OllamaChatClient
 //  - See instructions for sample code
+// Note that AzureOpenAIClient works with both GitHub Models and Azure OpenAI endpoints
+var aiEndpoint = builder.Configuration["AI:Endpoint"];
+if (string.IsNullOrEmpty(aiEndpoint))
+{
+    throw new InvalidOperationException("AI:Endpoint configuration is required.");
+}
+var aiKey = builder.Configuration["AI:Key"];
+if (string.IsNullOrEmpty(aiKey))
+{
+    throw new InvalidOperationException("AI:Key configuration is required.");
+}
+
+var innerChatClient = new AzureOpenAIClient(
+    new Uri(aiEndpoint),
+    new ApiKeyCredential(aiKey))
+    .GetChatClient("gpt-4o-mini").AsIChatClient();
+
+builder.Services.AddChatClient(innerChatClient);
 
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
